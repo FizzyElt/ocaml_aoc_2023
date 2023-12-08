8458c13f2fc077341c6dce76b3bdd6b554b95f50
FizzyElt <fizzyelt8786@gmail.com>
2023-12-08 23:05:50 +0800

    day8 part2 not passed

--- day8/main.ml
+++ day8/main.ml
@@ -50,7 +50,33 @@ let rec find_target_step target steps count node_map node =
     | Some n -> find_target_step target steps count node_map n
     | None -> count
 
-let part1_solution () =
+let rec find_all_steps (nodes : string option list) (steps : char list) node_map
+    count =
+  let is_all_endwith_z =
+    List.for_all (fun n_opt ->
+        n_opt
+        |> Option.map (fun n -> String.ends_with ~suffix:"Z" n)
+        |> Option.value ~default:false)
+  in
+  let rec aux steps nodes count =
+    match steps with
+    | step :: tail_steps ->
+        let next_nodes =
+          nodes
+          |> List.map (fun n_opt ->
+                 Option.bind n_opt (fun n -> next_node step n node_map))
+        in
+        if is_all_endwith_z next_nodes then (next_nodes, count + 1)
+        else aux tail_steps next_nodes (count + 1)
+    | [] -> (nodes, count)
+  in
+  let next_round_nodes, count = aux steps nodes count in
+  if List.exists Option.is_none next_round_nodes then
+    failwith "no possible path"
+  else if is_all_endwith_z next_round_nodes then count
+  else find_all_steps next_round_nodes steps node_map count
+
+let _part1_solution () =
   let lines = Sys.argv.(1) |> get_list_of_line Fun.id in
   let steps, tail =
     match lines with
@@ -61,4 +87,23 @@ let part1_solution () =
   let total = find_target_step "ZZZ" steps 0 node_map "AAA" in
   print_int total
 
-let () = part1_solution ()
+let part2_solution () =
+  let lines = Sys.argv.(1) |> get_list_of_line Fun.id in
+  let steps, tail =
+    match lines with
+    | steps :: _ :: tail -> (steps |> String.to_seq |> List.of_seq, tail)
+    | _ -> failwith "bad input"
+  in
+  let node_map = tail |> List.map parse_input |> create_map in
+  let start_nodes =
+    node_map |> SMap.to_seq
+    |> Seq.filter (fun (k, _) -> String.ends_with ~suffix:"A" k)
+    |> Seq.map (fun (k, _) -> Some k)
+    |> List.of_seq
+  in
+  print_endline "parse data done";
+  let total = find_all_steps start_nodes steps node_map 0 in
+
+  print_int total
+
+let () = part2_solution ()
