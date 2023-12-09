6dc3c95b91c6bd70861a906687dfb52f0b96016e
FizzyElt <fizzyelt8786@gmail.com>
2023-12-09 15:27:45 +0800

    day8 part 2

--- day8/main.ml
+++ day8/main.ml
@@ -50,31 +50,22 @@ let rec find_target_step target steps count node_map node =
     | Some n -> find_target_step target steps count node_map n
     | None -> count
 
-let rec find_all_steps (nodes : string option list) (steps : char list) node_map
-    count =
-  let is_all_endwith_z =
-    List.for_all (fun n_opt ->
-        n_opt
-        |> Option.map (fun n -> String.ends_with ~suffix:"Z" n)
-        |> Option.value ~default:false)
-  in
-  let rec aux steps nodes count =
-    match steps with
-    | step :: tail_steps ->
-        let next_nodes =
-          nodes
-          |> List.map (fun n_opt ->
-                 Option.bind n_opt (fun n -> next_node step n node_map))
-        in
-        if is_all_endwith_z next_nodes then (next_nodes, count + 1)
-        else aux tail_steps next_nodes (count + 1)
-    | [] -> (nodes, count)
-  in
-  let next_round_nodes, count = aux steps nodes count in
-  if List.exists Option.is_none next_round_nodes then
-    failwith "no possible path"
-  else if is_all_endwith_z next_round_nodes then count
-  else find_all_steps next_round_nodes steps node_map count
+let rec find_target_step2 steps count node_map node =
+  if String.ends_with ~suffix:"Z" node then count
+  else
+    let next_node, count =
+      List.fold_left
+        (fun (next, count) step ->
+          match next with
+          | Some n ->
+              if String.ends_with ~suffix:"Z" n then (None, count)
+              else (next_node step n node_map, count + 1)
+          | None -> (None, count))
+        (Some node, count) steps
+    in
+    next_node
+    |> Option.map (fun n -> find_target_step2 steps count node_map n)
+    |> Option.value ~default:count
 
 let _part1_solution () =
   let lines = Sys.argv.(1) |> get_list_of_line Fun.id in
@@ -98,12 +89,11 @@ let part2_solution () =
   let start_nodes =
     node_map |> SMap.to_seq
     |> Seq.filter (fun (k, _) -> String.ends_with ~suffix:"A" k)
-    |> Seq.map (fun (k, _) -> Some k)
+    |> Seq.map (fun (k, _) -> k)
+    |> Seq.map (fun k -> find_target_step2 steps 0 node_map k)
     |> List.of_seq
   in
-  print_endline "parse data done";
-  let total = find_all_steps start_nodes steps node_map 0 in
-
-  print_int total
+  List.iter (fun n -> Printf.printf "%d\n" n) start_nodes
 
 let () = part2_solution ()
+(* 68255921 *)
